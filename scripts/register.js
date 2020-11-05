8cad5f7a01cc83b97e5b6d07c84deae7834d185f
99002521 <swarnim.shikhar@ltts.com>
2020-11-05 17:38:47 +0530

    Final Validation Done

--- scripts/register.js
+++ scripts/register.js
@@ -1,27 +1,31 @@
-function validate(){
-	childname = document.getElementById("inputcname").value;
+function validata(){
+	childname = document.myform.inputcname.value;
 	password = document.myform.pass.value;
     age = document.myform.age.value;
     
-	if(childname ==""){
+	if(childname ==null){
 		alert("This field is required. Enter your child's name.");
-        //document.getElementById("inputcname").focus();
+        document.myform.inputcname.focus();
     }
-	// else if (password ==""||(!password.match(/[A-Z]/))){
-	// 	alert("enter your password");
-	// 	document.myform.pass.value="";
-	// 	document.myform.pass.focus();
-	// }else if (age ==""||isNaN(age)){
-	// 	alert("enter your age");
-	// 	document.myform.age.value="";
-	// 	document.myform.age.focus();
-	// }else if(document.myform.gender[0].checked==""
-	// 		&&document.myform.gender[1].checked==""){
-	// 	alert("enter gender");
-	// 	document.myform.gender[0].focus();
-    // }
+	 else if (password ==""||(!password.match(/[A-Z]/))){
+	 	alert("enter your password");
+	 	document.myform.pass.value="";
+	 	document.myform.pass.focus();
+	 }else if (age ==""||isNaN(age)){
+	 	alert("enter your age");
+	 	document.myform.age.value="";
+	 	document.myform.age.focus();
+	}else if(document.myform.gender[0].checked==""
+			&&document.myform.gender[1].checked==""){
+	 	alert("enter gender");
+		document.myform.gender[0].focus();
+    }
     else{
-	 	alert("You are registered")
-	 	document.myform.submit();
+	 	alert("You are registered");
+	 	document.myform.onsubmit();
 	 }
 }	
+
+function validate(){
+    alert("Thank you. Your child has been registered successfully! We will reach out to you soon.");
+}
