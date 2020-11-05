f84ee201fe00747c8148cc20d2efdf66a4910052
99002521 <swarnim.shikhar@ltts.com>
2020-11-05 17:21:30 +0530

    Changes

--- scripts/register.js
+++ scripts/register.js
@@ -1,17 +1,27 @@
-(function() {
-    'use strict';
-    window.addEventListener('load', function() {
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function(form) {
-    form.addEventListener('submit', function(event) {
-    if (form.checkValidity() === false) {
-    event.preventDefault();
-    event.stopPropagation();
+function validate(){
+	childname = document.getElementById("inputcname").value;
+	password = document.myform.pass.value;
+    age = document.myform.age.value;
+    
+	if(childname ==""){
+		alert("This field is required. Enter your child's name.");
+        //document.getElementById("inputcname").focus();
     }
-    form.classList.add('was-validated');
-    }, false);
-    });
-    }, false);
-    })();
+	// else if (password ==""||(!password.match(/[A-Z]/))){
+	// 	alert("enter your password");
+	// 	document.myform.pass.value="";
+	// 	document.myform.pass.focus();
+	// }else if (age ==""||isNaN(age)){
+	// 	alert("enter your age");
+	// 	document.myform.age.value="";
+	// 	document.myform.age.focus();
+	// }else if(document.myform.gender[0].checked==""
+	// 		&&document.myform.gender[1].checked==""){
+	// 	alert("enter gender");
+	// 	document.myform.gender[0].focus();
+    // }
+    else{
+	 	alert("You are registered")
+	 	document.myform.submit();
+	 }
+}	
